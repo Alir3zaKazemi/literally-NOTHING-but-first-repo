563a1b7eb659a89130295f7c03763880961264c0
Alir3za <alireza2212kazemi@gmail.com>
2021-08-22 15:43:32 +0430

    changes to l4.py

---
+++ l4.py
@@ -0,0 +1,13 @@
+
+my_list = [28,75,83,745,96,12,108,234]
+def mx(my_list):
+    max = 0
+    x = 0
+    y = len(my_list)
+    for i in range(y) :
+        if my_list[x] > max :
+            max = my_list[x]
+        x += 1
+    print(max)
+
+mx(my_list)
